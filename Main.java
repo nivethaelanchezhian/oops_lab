5556dbb6f6610d3a0cf44b34405931cb2133af01
Nivii <nivethae.cse25@ifet.ac.in>
2026-08-19 11:51:53 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,49 +1,36 @@
-import java.util.Scanner;
-import areaconversion.Area;
+abstract class Shape {
+    abstract void area();
+}
 
+// Circle class
+class Circle extends Shape {
+    double radius = 5;
+
+    void area() {
+        double a = 3.14 * radius * radius;
+        System.out.println("Area of Circle = " + a);
+    }
+}
+
+// Rectangle class
+class Rectangle extends Shape {
+    double length = 10;
+    double breadth = 5;
+
+    void area() {
+        double a = length * breadth;
+        System.out.println("Area of Rectangle = " + a);
+    }
+}
+
+// Main class
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        Area a = new Area();
+        Shape s1 = new Circle();
+        Shape s2 = new Rectangle();
 
-        System.out.println("AREA CONVERSION");
-        System.out.println("1. Square Meter to Square Feet");
-        System.out.println("2. Square Feet to Square Meter");
-        System.out.println("3. Square Meter to Square Yard");
-        System.out.println("4. Square Yard to Square Meter");
-
-        System.out.print("Enter your choice: ");
-        int choice = sc.nextInt();
-
-        System.out.print("Enter the area: ");
-        double value = sc.nextDouble();
-
-        switch (choice) {
-            case 1:
-                System.out.println("Result = " +
-                    a.squareMeterToSquareFeet(value) + " sq.ft");
-                break;
-
-            case 2:
-                System.out.println("Result = " +
-                    a.squareFeetToSquareMeter(value) + " sq.m");
-                break;
-
-            case 3:
-                System.out.println("Result = " +
-                    a.squareMeterToSquareYard(value) + " sq.yard");
-                break;
-
-            case 4:
-                System.out.println("Result = " +
-                    a.squareYardToSquareMeter(value) + " sq.m");
-                break;
-
-            default:
-                System.out.println("Invalid choice");
-        }
-
-        sc.close();
+        s1.area();
+        s2.area();
     }
 }
